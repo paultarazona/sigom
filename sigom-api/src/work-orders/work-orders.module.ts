dc405f8a3658b5d19a1f13133c14105045d58886
paultarazona <pmbenancio@gmail.com>
2026-07-16 10:18:47 -0500

    feat(api): publish closed SISCON orders through outbox

--- sigom-api/src/work-orders/work-orders.module.ts
+++ sigom-api/src/work-orders/work-orders.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WorkOrdersService } from './work-orders.service';
 import { WorkOrdersController } from './work-orders.controller';
+import { IntegrationsModule } from '../integrations/integrations.module';
 
 @Module({
   controllers: [WorkOrdersController],
+  imports: [IntegrationsModule],
   providers: [WorkOrdersService],
 })
 export class WorkOrdersModule {}
